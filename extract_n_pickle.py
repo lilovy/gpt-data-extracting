db11355a86914e2e1f04ab845d26063ecfa747ea
lilovy <iv1hanz@gmail.com>
2023-04-18 12:21:11 +0500

    main

--- extract_n_pickle.py
+++ extract_n_pickle.py
@@ -56,18 +56,18 @@ dt = """
 {"original": "Английский на уровне чтения документации", "simple_forms": [{"simple_form": "английский на уровне чтения документации", "tag": "knowledge"}]}, {"original": "Навыки монтажа СКС", "simple_forms": [{"simple_form": "навыки монтажа СКС", "tag": "skill"}]}, {"original": "доброжелательность, общительность, мобильность", "simple_forms": [{"simple_form": "доброжелательность", "tag": "unknown"}, {"simple_form": "общительность", "tag": "unknown"}, {"simple_form": "мобильность", "tag": "unknown"}]}, {"original": "web container", "simple_forms": [{"simple_form": "web container", "tag": "knowledge"}]}, {"original": "представление хозяйственной деятельности предприятия,", "simple_forms": [{"simple_form": "представление хозяйственной деятельности предприятия", "tag": "knowledge"}]}, {"original": "Redmine/Jira,", "simple_forms": [{"simple_form": "Redmine", "tag": "knowledge"}, {"simple_form": "Jira", "tag": "knowledge"}]}, {"original": "Ищешь стабильную работу;", "simple_forms": [{"simple_form": "стабильная работа", "tag": "unknown"}]}, {"original": "Опыт создания аналитических отчетов;", "simple_forms": [{"simple_form": "опыт создания аналитических отчетов", "tag": "skill"}]}, {"original": "Владение Kotlin, coroutines", "simple_forms": [{"simple_form": "владение Kotlin", "tag": "skill"}, {"simple_form": "владение coroutines", "tag": "skill"}]}, {"original": "Желательно наличие сертификата 1С8", "simple_forms": [{"simple_form": "сертификат 1С8", "tag": "unknown"}]}, {"original": "Навыки верстки документов;", "simple_forms": [{"simple_form": "навыки верстки документов", "tag": "skill"}]}, {"original": "понимание системы процессов организации;", "simple_forms": [{"simple_form": "понимание системы процессов организации", "tag": "knowledge"}]}, {"original": "Владение русским - приветствуется.", "simple_forms": [{"simple_form": "владение русским", "tag": "knowledge"}]}, {"original": "Знакомство с вычислительной геометрией", "simple_forms": [{"simple_form": "знакомство с вычислительной геометрией", "tag": "knowledge"}]}, {"original": "Навыки программирование АТС", "simple_forms": [{"simple_form": "навыки программирования АТС", "tag": "skill"}]}, {"original": "Знание ПК- высокий уровень", "simple_forms": [{"simple_form": "знание ПК", "tag": "knowledge"}]}, {"original": "Архитектуры SAS Customer Intelligence Studio", "simple_forms": [{"simple_form": "архитектура SAS Customer Intelligence Studio", "tag": "knowledge"}]}, {"original": "наличие коммуникативных навыков;", "simple_forms": [{"simple_form": "коммуникативные навыки", "tag": "skill"}]}, {"original": "имеет аналитический склад ума.", "simple_forms": [{"simple_form": "аналитический склад ума", "tag": "skill"}]}, {"original": "Опыт работы с Mercurial", "simple_forms": [{"simple_form": "опыт работы с Mercurial", "tag": "skill"}]}"""
 
 def save_data(data, filename = 'res.txt'):
-    with open(filename, 'a+') as f:
+    with open(filename, 'a+', encoding='utf-8') as f:
         if isinstance(data, list):
             for i in data:
-                json.dump(i, filename)
+                json.dump(i, f, ensure_ascii=False)
                 f.write('\n')
         else:
-            json.dump(data, f)
+            json.dump(data, f, ensure_ascii=False)
             f.write('\n')
 
 def extract_data(filename = 'res.txt'):
     data = []
-    with open(filename, 'r') as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         for l in f:
             data.append(json.loads(l))
     return data
@@ -93,9 +93,14 @@ def partial_request(data: list[str], num: int = 20):
 
 
 if __name__ == "__main__":
-    data = load_pkl('data.pkl')[200:500]
+    # data = load_pkl('data.pkl')[200:500]
+    data = extract_data()
+    for i in data:
+        print(i)
+        break
+    save_data(data, 'results.txt')
 
-    res = partial_request(data)
+    # res = partial_request(data)
 
     # for i in (extract_data()):
     #     print(i)
