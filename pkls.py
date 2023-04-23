2b30439fa6b5dbb4bb5b6405ae53dccdba500fce
lilovy <iv1hanz@gmail.com>
2023-04-23 20:49:34 +0500

    main

--- pkls.py
+++ pkls.py
@@ -10,26 +10,17 @@ def load_pkl(file):
     return data
 
 @timer
-def load_pkl_pandas(file) -> DataFrame:
+def load_pandas_pkl(file) -> DataFrame:
     df = pd.read_pickle(file)
     return df
-
-# data = load_pkl_pandas('src/.localdata/data.pkl')
-
-if __name__ == "__main__":
-    # data = load_pkl('src/.localdata/data.pkl')
-    # print(data[220100:220310])
-    print(len(data))
-    print(data.head(50))
-
-    # for i in data[400000:405000]:
-    #     print(i)
-# lens = [(len(x), x) for x in data]
-# print(max(lens))
-# for i in range(40):
-#     lens.remove(max(lens))
-#     print(max(lens))
 
 def pickling(data, file, mode = 'ab+'):
     with open(file, mode) as handle:
         pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+if __name__ == "__main__":
+    # data = load_pkl('.localdata/data_500k.pkl')
+    # print(data[220100:220310])
+    print(len(data))
+    print(data.head(50))
