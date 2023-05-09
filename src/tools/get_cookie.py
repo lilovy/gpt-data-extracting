69ddcf4c5d12e71a2da0d3f966bc9e651cd239e3
Savinukh <72504331+Savinukh@users.noreply.github.com>
2023-05-10 01:39:50 +0500

    Main

--- src/tools/get_cookie.py
+++ src/tools/get_cookie.py
@@ -2,76 +2,28 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from mail_reader import EmailReader, MailCriteria
-from time import sleep
+from tqdm import *
 
 
-# options = webdriver.FirefoxOptions()
-# options.add_argument('--marionette-port=0')
-# options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
-
-class ParserBing:
-    def __init__(self, user_agent: str):
-        firefox_options = webdriver.FirefoxOptions()
-        # firefox_options.headless = True
+class Browser:
+    def __init__(self):
+        # firefox_options = webdriver.FirefoxOptions()
+        # fire/fox_options.headless = True
         # firefox_options.add_argument('-private')
-        firefox_profile = webdriver.FirefoxProfile()    
-        firefox_profile.set_preference("general.useragent.override", user_agent)
-        # driver = webdriver.Firefox(executable_path=driver_path, firefox_profile=firefox_profile, options=options)
-        self.__driver = webdriver.Firefox(firefox_profile=firefox_profile, options=firefox_options)
-        self.__wait = WebDriverWait(self.__driver, 10) # Можешь поменять с 10 на 5 и протестить, просто интернет у меня плохой
+        self.__firefox_profile = webdriver.FirefoxProfile()    
+        self.__firefox_profile.set_preference("general.useragent.override", 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1788.0')
+        self.__driver = webdriver.Firefox(firefox_profile=self.__firefox_profile) #options=firefox_options
 
-    def sending_link(self, url: str='https://bing.com'):
+    def wait_clickable_element(self) -> WebDriverWait:
+        self.wait = WebDriverWait(self.__driver, 1)
+        return self.wait
+
+    def change_user_agent(self, user_agent: str):
+        self.__firefox_profile = webdriver.FirefoxProfile()    
+        self.__firefox_profile.set_preference("general.useragent.override", user_agent)
+                                              
+    def sending_link(self, url: str='https://login.live.com/'):
         self.__driver.get(url=url)
-
-    def __click_element(self, element_id: str, xpath: bool = False):
-        if xpath:
-            btn = self.__wait.until(EC.element_to_be_clickable((By.XPATH, element_id)))
-        else:
-            btn = self.__wait.until(EC.element_to_be_clickable((By.ID, element_id)))
-        btn.click()
-        # print(f'Нажата кнопка: {element_id}')
-
-    def __send_text(self, element_id: str, text: str, xpath: bool = False):
-        if xpath:
-            input_field = self.__wait.until(EC.element_to_be_clickable((By.XPATH, element_id)))
-        else:
-            input_field = self.__wait.until(EC.element_to_be_clickable((By.ID, element_id)))
-        input_field.send_keys(text)
-        # print(f'Введен текст: {element_id}')
-        # print(f'Текст: {text}')
-
-
-    def parser_bing(self, add_user: str, add_password: str, dict_id_elements: dict, not_error: bool = True): 
-        try:
-            for element in dict_id_elements:
-                sleep(1)
-                try:
-                    if element == 'iOttText' or element == 'idTxtBx_OTC_Password':
-                        self.__send_text(element, '')
-                        # print('элемент обнаружен')
-                        sleep(10)
-                        code = get_code_from_rambler(add_user, add_password)
-                        # print(code)
-                        self.__send_text(element, code)
-                    else:
-                        if dict_id_elements[element][0] == 'btn':
-                            if element.find('idSIButton9') != -1 or element.find('idBtn_Back') != -1:
-                                self.__click_element(element[1:],  dict_id_elements[element][1])  
-                            else:
-                                self.__click_element(element,  dict_id_elements[element][1])
-                                print(element)
-                            
-                        elif dict_id_elements[element][0] == 'txt':
-                            print(element)
-                            self.__send_text(element, dict_id_elements[element][1])
-                except:
-                    print(f'Пропуск элемента: {element}')
-                    continue
-            return not_error
-        except:
-            not_error = False
-            return not_error
     
     def get_cookies_from_bing(self):
         cookies = self.__driver.get_cookies()
@@ -79,64 +31,126 @@ class ParserBing:
     
     def close_driver(self):
         self.__driver.quit()
+
+
+class ElementText:    
+    def __init__(self, element: str, text: str, wait: WebDriverWait, is_xpath: bool = False):
+        self.element = element
+        self.__text = text
+        self.__is_xpath = is_xpath
+        self.__wait = wait
+
+    def action_element(self):
+        try:
+            if self.__is_xpath:
+                self.__input_field = self.__wait.until(EC.element_to_be_clickable((By.XPATH, self.element)))
+            else:
+                self.__input_field = self.__wait.until(EC.element_to_be_clickable((By.ID, self.element)))
+            self.__input_field.send_keys(self.__text)
+        except:
+            # print(f'Пропущен элемент: {self.element}')
+            return
+
+
+class ElementBtn: 
+    def __init__(self, element: str, wait: WebDriverWait, is_xpath: bool = False):
+        self.element = element
+        self.__is_xpath = is_xpath
+        self.__wait = wait
+ 
+    def action_element(self):
+        try:
+            if self.__is_xpath:
+                self.__btn = self.__wait.until(EC.element_to_be_clickable((By.XPATH, self.element)))
+            else:
+                self.__btn = self.__wait.until(EC.element_to_be_clickable((By.ID, self.element)))
+            self.__btn.click()
+        except:
+            # print(f'Пропущен элемент: {self.element}')
+            return
+
+    def is_check_btn(self):
+        try:
+            if self.__wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[2]/div/div[2]/div/div[1]/div[2]/div/div/div/div/div/div[1]/div/div/div/a'))):
+                return True
+        except:
+            return False
     
 
-def get_code_from_rambler(user: str, password: str):
-     with EmailReader(
-        client="imap.rambler.ru", 
-        email_address=user, 
-        password=password,
-        ) as reader:
+browser = Browser()
 
-        body = reader.get_code_from_email(
-            sender_email="account-security-noreply@accountprotection.microsoft.com",
-            criteria=MailCriteria.UNSEEN
-            )
-        if body:
-            return body
-        else:
-            return get_code_from_rambler(user, password)
+browser.sending_link()
+wait = browser.wait_clickable_element()
+
 
 def get_dict_id_elements(user: str, password: str, add_user: str, add_password: str):
     dict_id_elements = {
-        'id_s' : ['btn', False],
-        # 'bnp_btn_reject' : ['btn', False],
-        'i0116' : ['txt', user],
+        'i0116' : ['txt', user, False],
         '1idSIButton9' : ['btn', False],
-        'i0118' : ['txt', password],
+        'i0118' : ['txt', password, False],
         '2idSIButton9' : ['btn', False],
-        'iProofEmail' : ['txt', add_user],
+        'iProofEmail' : ['txt', add_user, False],
         'iSelectProofAction' : ['btn', False],
-        'iOttText' : ['txt', 'code'],
+        'iOttText' : ['txt', 'code', False],
         'iVerifyCodeAction' : ['btn', False],
+        '1id__0' : ['btn', False],
+        '2id__0' : ['btn', False],
         '1idBtn_Back' : ['btn', False],
-        'idTxtBx_OTC_Password' : ['txt', 'code'],
+        'idTxtBx_OTC_Password' : ['txt', 'code', False],
         '3idSIButton9' : ['btn', False],
         '2idBtn_Back' : ['btn', False],
+        '/html/body/div[2]/div/div[2]/div[2]/div[2]/div/div/div[3]/div/div[2]/a[1]' : ['btn', True],
+        '/html/body/div[1]/div[2]/div/div[2]/div/div[1]/div[2]/div/div/div/div/div/div[1]/div/div/div/a' : ['btn', True],
+        '/html/body/header/nav/ul/li[2]/a' : ['btn', True],
         }   
     return dict_id_elements
 
-def get_cookie(user, password, add_user, add_password, user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1788.0'):
-    not_error = True
-    cookie = ['not cookie']
-    dict_id_elements = get_dict_id_elements(user, password, add_user, add_password)
-    browser = ParserBing(user_agent=user_agent)
-    browser.sending_link()
-    not_error = browser.parser_bing(add_user, add_password, dict_id_elements)
-    print(not_error)
-    if not_error:
+
+def create_list_object_element(dict_marked_elements: dict, wait: WebDriverWait) -> list[object]:
+    list_element_object = []
+    for element in dict_marked_elements:
+        if dict_marked_elements[element][0] == 'btn':
+            if element.find('idSIButton9') != -1 or element.find('idBtn_Back') != -1 or element.find('id__0') != -1:
+                 el_btn = ElementBtn(element[1:], wait, dict_marked_elements[element][1])
+            else:
+                el_btn = ElementBtn(element, wait, dict_marked_elements[element][1])
+            list_element_object.append(el_btn)
+
+        elif dict_marked_elements[element][0] == 'txt':
+            el_txt = ElementText(element, dict_marked_elements[element][1], wait, dict_marked_elements[element][2])
+            list_element_object.append(el_txt)
+    return list_element_object
+
+def parser_bing(list_objects: list[object]):
+        is_parser = True
+        while is_parser:
+            for object_el in tqdm(list_objects, total=len(list_objects)):
+                if isinstance(object_el, ElementBtn):
+                    if object_el.is_check_btn():
+                        browser.sending_link('https://bing.com/chat')
+                        is_parser = False
+                        break
+                object_el.action_element()
         cookie = browser.get_cookies_from_bing()
-    browser.sending_link('https://bing.com/chat')
-    browser.close_driver()
+        browser.close_driver()
+        return cookie
+
+def get_cookie(email: str, password: str, second_email: str, second_password : str, wait: WebDriverWait):
+    dict_elements = get_dict_id_elements(email, password, second_email, second_password)
+    list_objects = create_list_object_element(dict_elements, wait)
+    cookie = parser_bing(list_objects)
     return cookie
-    
+      
 
 if __name__ == '__main__':
-    user = 'conclemnae@outlook.com'
-    password = 'g2vMOUxhLn'
-    add_user = 'spadgerthankpa1999@ro.ru'
-    add_password = 'sdWj8ixDA'
-    cookie = get_cookie(user, password, add_user, add_password)
+    email = 'ehleuveltomcbu@outlook.com'
+    password = '3WkAQqPh1i'
+    second_email = 'salugihi@rambler.ru'
+    second_password = '9gNqXGwT4M'
+
+    cookie = get_cookie(email, password, second_email, second_password, wait)
     print(cookie)
+
+
 
 
